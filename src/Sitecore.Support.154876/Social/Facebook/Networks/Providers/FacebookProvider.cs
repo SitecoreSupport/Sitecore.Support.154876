396504842c07e19afa5721fcf9c339af1de02077
Andrey Krupskiy <anr@sitecore.net>
2017-04-07 18:06:09 +0300

    initial commit

---
+++ src/Sitecore.Support.154876/Social/Facebook/Networks/Providers/FacebookProvider.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Facebook;
+using Newtonsoft.Json.Linq;
+using Sitecore.Social.Facebook.Connector.Managers;
+using Sitecore.Social.Facebook.Connector.Paths;
+using Sitecore.Social.Facebook.Exceptions.Analyzers;
+using Sitecore.Social.Infrastructure.Utils;
+using Sitecore.Social.NetworkProviders.Args;
+using Sitecore.Social.NetworkProviders.Connector.Paths;
+using Sitecore.Social.NetworkProviders.Interfaces;
+using Sitecore.Web;
+
+namespace Sitecore.Support.Social.Facebook.Networks.Providers
+{
+  public class FacebookProvider : Sitecore.Social.Facebook.Networks.Providers.FacebookProvider, IAuth
+  {
+    public FacebookProvider(Sitecore.Social.NetworkProviders.Application application) : base(application)
+    {
+    }
+
+    void IAuth.AuthGetAccessToken(AuthArgs args)
+    {
+      HttpRequest request = HttpContext.Current.Request;
+      if (string.IsNullOrEmpty(request.QueryString.Get("error")))
+      {
+        string str2 = request.QueryString.Get("code");
+        if (!string.IsNullOrEmpty(str2))
+        {
+          string fullUrl = WebUtil.GetFullUrl("/layouts/system/Social/Connector/SocialLogin.ashx?type=access");
+          WebRequestManager manager =
+            new WebRequestManager(string.Format(CultureInfo.CurrentCulture,
+              FacebookPathsFactory.Facebook.API.AccessToken +
+              "?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}",
+              new object[] {args.Application.ApplicationKey, fullUrl, args.Application.ApplicationSecret, str2}));
+          string response = manager.GetResponse();
+          if (response != null)
+          {
+            JToken token;
+            JToken token2;
+            JObject obj2 = JObject.Parse(response);
+            if (!obj2.TryGetValue("access_token", out token))
+            {
+              throw new FacebookOAuthException("Access token was not present in the response.");
+            }
+            DateTime? nullable = null;
+            DateTime? nullable2 = null;
+            if (obj2.TryGetValue("expires_in", out token2))
+            {
+              nullable = new DateTime?(DateTime.UtcNow);
+              nullable2 = new DateTime?(nullable.Value.AddSeconds((double) token2.Value<int>()));
+            }
+            AuthCompletedArgs authCompletedArgs = new AuthCompletedArgs
+            {
+              Application = args.Application,
+              AccessTokenSecret = token.Value<string>(),
+              CallbackPage = args.CallbackUrl,
+              ExternalData = args.ExternalData,
+              AttachAccountToLoggedInUser = args.AttachAccountToLoggedInUser,
+              IsAsyncProfileUpdate = args.IsAsyncProfileUpdate,
+              AccessTokenSecretIssueDate = nullable,
+              AccessTokenSecretExpirationDate = nullable2
+            };
+            if (!string.IsNullOrEmpty(args.CallbackType))
+            {
+              base.InvokeAuthCompleted(args.CallbackType, authCompletedArgs);
+            }
+          }
+          else
+          {
+            new FacebookExceptionAnalyzer().Analyze(manager.ErrorText);
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.154876/Social/Pipelines/Loader/UpdateFacebookPaths.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+using Sitecore.Social.Facebook.Connector.Paths;
+using Sitecore.Social.NetworkProviders.Connector.Paths;
+
+namespace Sitecore.Support.Social.Pipelines.Loader
+{
+  public class UpdateFacebookPaths
+  {
+    public void Process(PipelineArgs args)
+    {
+      var facebook = new FacebookPaths("http://www.facebook.com/home.php", new SocialPaths.CommandsPaths("type=facebook_auth", "type=facebook_access", "type=facebook_connect", "type=facebook_add", "type=facebook_remove"), new FacebookPaths.Links("https://www.facebook.com/v2.5/dialog/oauth", "https://graph.facebook.com/v2.5/oauth/access_token", "/v2.5/{0}/accounts", "/v2.5/{0}/feed", "/v2.5/me", "/v2.5/{0}", "/v2.5/{0}/comments", "v2.5", "/v2.5/me/ids_for_business"));
+      typeof(FacebookPathsFactory).GetField("Facebook").SetValue(null, facebook);
+      Log.Info("DEBUG 154876:"+FacebookPathsFactory.Facebook.API.AccessToken, this);
+    }
+  }
+}
